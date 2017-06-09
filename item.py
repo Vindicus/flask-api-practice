db92c1194b8374f565a66a52aa61490f812ae71f
Vindicus <pyang1016@gmail.com>
2017-06-09 03:26:14 +0000

    add select statement to retrieve all items

--- item.py
+++ item.py
@@ -42,7 +42,7 @@ class Item(Resource):
         try:
             self.insert(item)
         except:
-            return {"message":"an error occurred"}
+            return {"message":"an error occurred"},500
             
         return item, 201
     
@@ -65,16 +65,36 @@ class Item(Resource):
     def put(self,name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name':name,'price':data['price']}
         if item is None:
-            item = {'name':name,'price':data['price']}
-            items.append(item)
+            self.insert(updated_item)
+            
         else:
-            item.update(data)
-        return item
+            self.update(updated_item)
+        return updated_item
+    
+    @classmethod
+    def update(cls,item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query,(item['price'],item['name']))
+        connection.commit()
+        connection.close()
+        
         
         
 class ItemList(Resource):
     def get(self):
-        return {'items': items}
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+            
+        connection.close()
         
+        return {'items': items}
