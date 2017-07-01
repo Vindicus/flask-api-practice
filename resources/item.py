bd8beeb45579beadac1e3bb77b9a9b15676b2379
Vindicus <pyang1016@gmail.com>
2017-07-01 02:03:59 +0000

    displaying ItemList resource

--- resources/item.py
+++ resources/item.py
@@ -58,14 +58,5 @@ class Item(Resource):
         
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-            
-        connection.close()
+        return {'items': [Item.json() for item in ItemModel.query.all()]}
         
-        return {'items': items}
