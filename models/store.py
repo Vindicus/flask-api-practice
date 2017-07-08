9338da97f44b7c003f67525f520d4b3860a8599c
Vindicus <pyang1016@gmail.com>
2017-07-08 05:42:20 +0000

    create StoreModel

---
+++ models/store.py
@@ -0,0 +1,10 @@
+import sqlite3
+import db from db
+
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    
+    items = db.relationship('ItemModel', lazy='dynamic')
